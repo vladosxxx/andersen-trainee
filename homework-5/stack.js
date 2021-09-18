65b55e15ed5212a52279b87bba6d13088bf2bb8f
vladosxxx <vladosxxx@gmail.com>
2021-09-18 12:04:03 +0300

    hm 5 fix 3

--- homework-5/stack.js
+++ homework-5/stack.js
@@ -1,20 +1,21 @@
 class StackElem {
-  constructor(el) {
+  constructor(el, nextEl) {
     this.el = el
-    this.nextEl = null
+    this.nextEl = nextEl
   }
 }
 class Stack {
   constructor(maxStackSize = 10) {
     if (typeof maxStackSize !== 'number' || !isFinite(maxStackSize))
       throw new Error('Error')
-    this.stackHead = new StackElem(null)
+    this.stackHead = new StackElem(null, null)
     this.stackSize = 0
     this.maxStackSize = maxStackSize
+    console.log(this.stackHead)
   }
   push(elem) {
     if (this.maxStackSize < this.stackSize) throw new Error('Error')
-    let newEl = new StackElem(elem)
+    let newEl = new StackElem(elem, null)
     let curEl = this.stackHead
     this.stackHead = newEl
     newEl.nextEl = curEl
